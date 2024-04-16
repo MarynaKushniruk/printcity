99fe188f02f56cbd7ba23723379e53d2e35a718c
marinakusniruk <Kushnirukmv@gmail.com>
2024-04-16 10:39:54 +0200

    Programme completed

--- src/main/kotlin/com/example/printcity/PrintcityApplication.kt
+++ src/main/kotlin/com/example/printcity/PrintcityApplication.kt
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 @SpringBootApplication
 class PrintcityApplication
 
-fun main(args: Array<String>) {
+fun main() {
    println("Enter your city..")
-    val city = readln();
-    println("User lives in $city!");
+    val city = readln()
+    println("User lives in $city!")
 }
